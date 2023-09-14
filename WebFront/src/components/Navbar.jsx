80e0d45fe21c8fcbfca2f1633d414f5351ec48d7
marianacy <mariana.eli.corona@gmail.com>
2023-09-14 10:36:07 -0600

    Cambios a los enlaces del navbar

--- WebFront/src/components/Navbar.jsx
+++ WebFront/src/components/Navbar.jsx
@@ -7,22 +7,22 @@ import { navlinks } from '../constants';
 const Navbar = () => {
 
   // Elemento activo
-  const [active, setActive] = useState("Home");
+  const [active, setActive] = useState("Inicio");
   // Mostrar/ocultar elementos
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className='bg-secondary w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="logo"className="w-[220px] h-[25px] ml-5"/>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="logo"className="w-[170px] h-[25px]"/>
 
       {/* Mostrar los enlaces */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navlinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+            className={`font-poppins font-medium cursor-pointer text-[16px] ${
               active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navlinks.length - 1 ? "mr-5" : "mr-10"}`}
+            } ${index === navlinks.length - 1 ? "mr-0" : "mr-10"}`}
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
@@ -40,7 +40,7 @@ const Navbar = () => {
       <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-secondary absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-orange-500 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navlinks.map((nav, index) => (
@@ -58,7 +58,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
