135b06e2e32d1ad1d5d8576277d8723e3d3667b6
marianacy <mariana.eli.corona@gmail.com>
2023-09-14 10:42:24 -0600

    Nuevas secciones agregadas

--- WebFront/src/App.jsx
+++ WebFront/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import styles from './styles'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
+    
+    <div className="bg-gradient-to-r from-secondary to-orange-400 w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`bg-gradient-to-r from-secondary to-orange-400 ${styles.boxWidth}`}>
+          <Navbar />
+        </div>   
+      </div>
+
+      <div className={`bg-gradient-to-r from-secondary to-orange-400 ${styles.flexStart}`}>
+        <div className={`bg-gradient-to-r from-secondary to-orange-400 ${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-white ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Services/>
+        </div>
+      </div>
     </div>
   )
 }
