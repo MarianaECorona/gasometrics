f26e6063097c613b0b87b741c1772a8f72e18ba5
marianacy <mariana.eli.corona@gmail.com>
2023-09-15 00:13:51 -0600

    banner añadido

--- WebFront/src/components/Hero.jsx
+++ WebFront/src/components/Hero.jsx
@@ -1,14 +1,15 @@
 import styles from "../styles"
+import img from "../assets/bannerC-hd.png"
 
 
 const Hero = () =>{
     return(
         <div id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
             <div className="flex flex-row justify-between items-center w-full">
-                <h1 className="flex-1 font-poppins font-semibold ss:text-[52px] text-[32px] text-white ss:leading-[100.8px] leading-[75px] ml-5">
+                <h1 className="flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] ml-5">
                     No te quedes sin<br className="sm:block hidden" />{" "}
 
-                    <span className="text-gradient ss:text-[72px] text-[52px]"> El Gas</span>{" "}
+                    <span className="text-gradient ss:text-[100px] text-[75px]"> El Gas</span>{" "}
                     
                     <p className={`${styles.paragraph} max-w-[400px] text-white mt-5`}>
                     Revisa desde cualquier dispositivo el nivel restante gas 
@@ -16,9 +17,9 @@ const Hero = () =>{
                     </p>
                 </h1>
             </div>
-                {/* Imagen de la portada */}
+                <img src={img} alt="banner" className="w-[100%] h-[100%] md:w-[50%] md:h-[50%] relative" />
             <div>
-               
+                           
             </div>
         </div>
     );
