862b465b04fda436824d13bf45f2b3aa3748f4c1
marianacy <mariana.eli.corona@gmail.com>
2023-09-17 13:11:48 -0600

    servicios

--- WebFront/src/components/Services.jsx
+++ WebFront/src/components/Services.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import styles, { layout } from "../styles"
+import tanque from "../assets/tanque.png"
+import shape from "../assets/shape1.svg"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id="services" className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2} text-secondary mt-3 ml-4`}>
+          Conoce el nivel restante <br className="sm:block hidden" />
+          de gas en tu tanque
+        </h2>
+        <p className={`${styles.paragraph} text-slate-900 mt-1 ml-4`}>
+          Nuestro sistema puede saber con precisión el nivel que resta <br className="sm:block hidden"/> 
+          en tu tanque estacionario con <b className={`text-secondary`}>inteligencia artificial</b>
+        </p>
+      </div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`} >
+        <img src={tanque} alt="tanque estacionario" className="w-[80%] h-[80%] relative z-[5]"/>
+        <div className="absolute z-[0] w-[90%] h-[95%]">
+          <img src={shape} alt="shape"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
