026fe9bca3fa8807b3e3e8894ea6f66603f95da8
marianacy <mariana.eli.corona@gmail.com>
2023-09-23 20:47:50 -0600

    modulos agregados

--- WebFront/src/App.jsx
+++ WebFront/src/App.jsx
@@ -2,6 +2,9 @@ import styles from './styles'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
+import ExtraService from './components/ExtraService'
+import Partness from './components/Partness'
+import GetStarted from './components/GetStarted'
 
 const App = () => {
   return (
@@ -16,12 +19,37 @@ const App = () => {
       <div className={`${styles.flexStart}`}>
         <div className={` ${styles.boxWidth}`}>
           <Hero />
+          <div className="text-center mt-5 bg-white text-secondary">
+          </div>
         </div>
       </div>
 
       <div className={`bg-white ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
+          <h1 className={`justify-center ${styles.heading1} text-secondary flex mt-5`}>
+              Te Ofrecemos
+          </h1>
           <Services/>
+        </div>
+      </div>
+      <div className={`bg-white ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <ExtraService/> 
+        </div>
+      </div>
+
+      <div className={`bg-white ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <h1 className={`justify-center ${styles.heading1} text-secondary flex mt-10`}>
+            Nuestros Socios
+          </h1>
+          <Partness/>
+        </div>
+      </div> 
+
+      <div className={`${styles.flexStart} bg-white`}>
+        <div className={`${styles.boxWidth}`}>
+          <GetStarted/>
         </div>
       </div>
     </div>
