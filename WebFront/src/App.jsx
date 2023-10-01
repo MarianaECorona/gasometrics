74bb88e84d02e01de40e1c3e474b406b215471ca
marianacy <mariana.eli.corona@gmail.com>
2023-09-30 22:14:14 -0600

    ruta al registro

--- WebFront/src/App.jsx
+++ WebFront/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Index from './Index'
 import Login from './views/Login'
+import Register from './views/Register'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Index/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   
