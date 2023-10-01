cefbfc4b8978ef4a7ecd0d6b3f9d146cd724e75d
marianacy <mariana.eli.corona@gmail.com>
2023-10-01 12:45:19 -0600

    archivo con formulario

--- WebFront/src/views/SignUp.jsx
+++ WebFront/src/views/SignUp.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import styles from "../styles"
+import Form from "../components/SingUp/Form"
 
 const SignUp = () => {
   return (
-    <div>Registro</div>
+    <div className="bg-gradient-to-r from-secondary to-orange-400 flex w-full h-screen">
+      <div className={`w-full ${styles.flexCenter}  lg:w-3/3`}>
+        <Form/>
+      </div>
+    </div>
   )
 }
 
