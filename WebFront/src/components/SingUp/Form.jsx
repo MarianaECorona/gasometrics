1285a5b2f022e780cdd3638ba30253cd3fc88fae
marianacy <mariana.eli.corona@gmail.com>
2023-10-01 12:43:42 -0600

    form titulos

---
+++ WebFront/src/components/SingUp/Form.jsx
@@ -0,0 +1,21 @@
+import styles,{layout} from "../../styles"
+
+const Form = () => {
+  return (
+    
+    <div className="w-11/12 max-w-[700px] px-10 py-5 rounded-3xl bg-white border-2 border-gray-100">
+        <h2 className={`${styles.heading2} text-5x1 text-secondary`}>
+          Crear una cuenta
+        </h2>
+        <p className={`${styles.paragraph} text-slate-700`}>
+          Esto tomará poco tiempo
+        </p>
+        <form action="">
+          0
+        </form>
+        Form
+    </div>
+  )
+}
+
+export default Form
