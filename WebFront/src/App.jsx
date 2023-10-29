4cc8ab7ee07122e0f64f81e1d65b497f7d054204
marianacy <mariana.eli.corona@gmail.com>
2023-10-29 15:06:16 -0600

    archivos varios

--- WebFront/src/App.jsx
+++ WebFront/src/App.jsx
@@ -1,19 +1,20 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Index from './Index'
 import Login from './views/Login'
+import Register from './views/SignUp'
 
 const App = () => {
   return (
     
-   
-      <BrowserRouter>
-        <Routes>
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<Index/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
-    
-  )
+  
+  );
 }
 
 export default App
