ae99a4dc28b8fdd46c7b97f60c8e5391e38a8ef3
marianacy <99087622+MarianaECorona@users.noreply.github.com>
2024-02-28 19:09:43 -0600

    Delete administrador directory

--- administrador/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Administrador(models.Model):
-    class Meta:
-        db_table='Administrador'
-    id_administrador = models.BigAutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    contrasenia = models.CharField(max_length=254)
-    correo = models.EmailField(max_length=254)
