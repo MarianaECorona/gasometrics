34157c31f3b9d31e33c453103d9aee5302da5709
Jessalprez <jessalprez1@hotmail.com>
2024-05-07 23:57:48 -0600

    nueva llave

--- gasometrics/settings.py
+++ gasometrics/settings.py
@@ -220,4 +220,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  
 EMAIL_HOST_USER = 'gasometricsmx@gmail.com'
-EMAIL_HOST_PASSWORD = 'dhtruozgyrwwjgzp'
+EMAIL_HOST_PASSWORD = 'ocbpbftclzwbthhb'
